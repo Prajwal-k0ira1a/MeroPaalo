65469c7ce0ffbe79dcc2a2d23a831db91a6686e0
shresthasubin <shresthasubin938@gmail.com>
2026-02-23 22:11:03 +0545

    connecting with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './src/db/index.js';
+
+dotenv.config({
+    path: './.env'
+})
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to intra college hackathon")
 })
 
-app.listen(port, () => {
-    console.log(`App is listening at http://localhost:${port}`)
+connectDB()
+    .then(() => {
+    app.on('Error', (err) => {
+        console.log('Failed to listen', err)
+        process.exit(1)
+    })
+
+    app.listen(port, () => {
+        console.log(`App is listening at http://localhost:${port}`)
+    })
 })
+    .catch(err => {
+        console.log("Error connecting DB,", err.message)
+        process.exit(1)
+    })
+
