b5ef308dca0084154913a2a22f0d7e84ca8ed986
Prajwal-k0ira1a <prajwalkoirala05@gmail.com>
2026-02-24 22:06:14 +0545

    refactor : Browser Router redirection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import LandingPage from './Landing Page/LandingPage';
-
+import DashboardPage from './StaffAdmin/DashboardPage';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/staff-admin" element={<DashboardPage />} />
     </Routes> 
   );
 }
