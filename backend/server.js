45dd9ed7ef10af3955cd85ecf302dd35ca332409
shresthasubin <shresthasubin938@gmail.com>
2026-02-25 11:03:27 +0545

    adding cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./src/database/index.js";
 import { notFound, errorHandler } from "./src/middlewares/error.middleware.js";
@@ -16,6 +17,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("Queue backend running");
