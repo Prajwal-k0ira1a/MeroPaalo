8b520a6fb1814a4a0b7be3da4d6d5f1462128a65
Sujal Shrestha <sujal84071@gmail.com>
2026-02-26 14:02:36 +0545

    Fixed CORS Error & added vercel URL in .env

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL || "http://localhost:5173",
+    process.env.CLIENT_URL,
   ],
   credentials: true,
 }));
